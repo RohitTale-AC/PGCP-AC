3a6727f85d1fed581353ccea4a81441bb405eb44
RohitTale-AC <rohit.tale.cmaug26@gmail.com>
2026-09-16 19:05:39 +0530

    Submitted

---
+++ Assignments/OOPJ Module/Assignement3/StringAssignemnt/Question3.java
@@ -0,0 +1,16 @@
+
+
+public class Question3 {
+
+	public static void main(String[] args) {
+
+		// String ends with the contents of another string.
+
+		String str1 = "Python Exercises";
+		String str2 = "Python Exercise";
+
+		System.out.println("\"Python Exercises\" ends with \"se\"? " + (str1.endsWith("se")));
+		System.out.println("\"Python Exercise\" ends with \"se\"? " + (str2.endsWith("se")));
+	}
+
+}
---
+++ Assignments/OOPJ Module/Assignement3/StringAssignemnt/Question7.java
@@ -0,0 +1,19 @@
+
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// Java program to reverse a string.
+
+		String str = "The quick brown fox jumps";
+
+		System.out.println("The given string is: " + str);
+		System.out.println("The string in reverse order is: ");
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			System.out.print(str.charAt(i));
+		}
+
+	}
+
+}
